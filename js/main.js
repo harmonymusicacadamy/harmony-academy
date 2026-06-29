4297e251010f3373696234f3c032fdf5c1f8023c
harmonymusicacadamy <harmonymusicacadamy@gmail.com>
2026-06-29 08:16:18 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -107,9 +107,7 @@ function renderFooter() {
       <div>
         <h4>Get in touch</h4>
         <ul>
-          <li><a href="mailto:hello@harmonymusicacademy.com">hello@harmonymusicacademy.com</a></li>
-          <li><a href="tel:+10000000000">+1 (000) 000-0000</a></li>
-          <li><span>Studio address goes here</span></li>
+          <li><a href="mailto:harmonymusicacadamy@gmail.com">harmonymusicacadamy@gmail.com</a></li>
         </ul>
       </div>
     </div>
